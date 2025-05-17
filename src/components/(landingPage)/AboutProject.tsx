d88ce1da0056a127aa99b0cc3b2494088b9878ed
habibzia007 <habibziabwn@gmail.com>
2025-05-17 20:51:39 +0500

    Landing page image

--- src/components/(landingPage)/AboutProject.tsx
+++ src/components/(landingPage)/AboutProject.tsx
@@ -7,16 +7,12 @@ export default function AboutProject() {
     return (
         <section id="projects" className="w-full px-2 sm:px-4 py-6 sm:py-12 bg-white relative z-40">
             <div className="max-w-7xl mx-auto">
-                {/* Web View Section */}
                 <div className="mb-16">
-                    {/* Web View Header */}
                     <div className="flex justify-center items-center my-6 sm:my-12">
                         <div className="h-[3px] sm:h-[4px] rounded bg-[#FF7C2B] w-16 sm:w-32 md:w-64"></div>
                         <span className="mx-2 sm:mx-4 text-[#FF7C2B] text-xl sm:text-2xl md:text-[28px] font-[300px]">Web View</span>
                         <div className="h-[3px] sm:h-[4px] rounded bg-[#FF7C2B] w-16 sm:w-32 md:w-64"></div>
                     </div>
-
-                    {/* Web View Image Container */}
                     <div className="w-full flex justify-center mt-4 sm:mt-8">
                         <div className="w-full max-w-full sm:max-w-5xl px-1 sm:px-4">
                             <Image
